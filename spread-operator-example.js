63935ea242ff948dc5546d2147c0dbc6a839fbce
lebinhthuan <104605059+lebinhthuan@users.noreply.github.com>
2022-06-01 17:13:08 +0700

    update-buoc 3

--- spread-operator-example.js
+++ spread-operator-example.js
@@ -1,7 +1,11 @@
-// Example 2: Use spread operator for merging arrays
-const cars1 = ["AUDI","BMW","TATA","MERCEDES"];
-const cars2 = ["NISSAN","TOYOTA"];
-// copied elements from cars1 and cars2 to cars3 here
-const cars3 = [...cars1, ...cars2];
 
-console.log(cars3);
+// Example 3: We will use the spread operator with objects
+// cars1 object
+const cars1 = {
+    Brand : "BMW",
+    Color : "RED"
+}
+// copy cars1 object using spread
+// operator to create cars2 object
+const cars2 = {...cars1};
+console.log(cars2);
