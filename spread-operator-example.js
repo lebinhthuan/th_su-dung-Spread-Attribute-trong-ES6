9308019ed4e17a055f8667a2efc9263ed21faa33
lebinhthuan <104605059+lebinhthuan@users.noreply.github.com>
2022-06-01 17:19:11 +0700

    Update - buoc 6

--- spread-operator-example.js
+++ spread-operator-example.js
@@ -1,7 +1,9 @@
-
-
-
-// Example 5: Use the spread operator to split the string into an array of strings.
-const car = "AUDI";
-const a = [...car];
+// Example 6: Use the spread operator with array destructuring.
+const numbers = [3,5,7,8,9];
+// Here we assign a,b and c with 3,5 and 7,
+// the rest of the elements will all go to others
+const [a,b,c,...others] = numbers;
 console.log(a);
+console.log(b);
+console.log(c);
+console.log(others);
